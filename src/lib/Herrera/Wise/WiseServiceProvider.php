acae358bd594c1f54ea3caed2c72422131cd5058
Kevin Herrera <kevin@herrera.io>
2013-04-09 16:03:06 -0700

    Adding current work.

---
+++ src/lib/Herrera/Wise/WiseServiceProvider.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Herrera\Wise;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+
+/**
+ * Registers Wise as a service.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class WiseServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function boot(Application $app)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function register(Application $app)
+    {
+        $app['wise'] = $app->share(
+            function () use ($app) {
+                $wise = new Wise($app['debug']);
+                $wise->setLoader($app['wise.loader']);
+                $wise->setCollector($app['wise.collector']);
+                $wise->setProcessor($app['wise.processor']);
+
+                if (isset($app['wise.cache_dir'])) {
+                    $wise->setCacheDir($app['wise.cache_dir']);
+                }
+
+                return $wise;
+            }
+        );
+
+        $app['wise.collector'] = $app->share(
+            function () use ($app) {
+                return new Resource\ResourceCollector();
+            }
+        );
+
+        $app['wise.loader'] = $app->share(
+            function () use ($app) {
+                return new DelegatingLoader($app['wise.loader_resolver']);
+            }
+        );
+
+        $app['wise.loader_resolver'] = $app->share(
+            function () use ($app) {
+                return new LoaderResolver($app['wise.loaders']);
+            }
+        );
+
+        $app['wise.loaders'] = $app->share(
+            function () use ($app) {
+                $loaders = array(
+                    new Loader\IniFileLoader($app['wise.locator']),
+                    new Loader\PhpFileLoader($app['wise.locator']),
+                );
+
+                if (class_exists('Herrera\\Json\\Json')) {
+                    $loaders[] = new Loader\JsonFileLoader(
+                        $app['wise.locator']
+                    );
+                }
+
+                if (class_exists('DOMDocument')) {
+                    $loaders[] = new Loader\XmlFileLoader($app['wise.locator']);
+                }
+
+                if (class_exists('Symfony\\Component\\Yaml\\Yaml')) {
+                    $loaders[] = new Loader\YamlFileLoader(
+                        $app['wise.locator']
+                    );
+                }
+
+                return $loaders;
+            }
+        );
+
+        $app['wise.locator'] = $app->share(
+            function () use ($app) {
+                return new FileLocator($app['wise.path']);
+            }
+        );
+
+        $app['wise.processor'] = $app->share(
+            function () use ($app) {
+                return new Processor\DelegatingProcessor(
+                    $app['wise.processor_resolver']
+                );
+            }
+        );
+
+        $app['wise.processor_resolver'] = $app->share(
+            function () use ($app) {
+                return new Processor\ProcessorResolver($app['wise.processors']);
+            }
+        );
+
+        $app['wise.processors'] = array();
+    }
+}
---
+++ src/tests/Herrera/Wise/Tests/WiseServiceProviderTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Herrera\Wise\Tests;
+
+use Herrera\PHPUnit\TestCase;
+use Herrera\Wise\WiseServiceProvider;
+use Silex\Application;
+
+class WiseServiceProviderTest extends TestCase
+{
+    /**
+     * @var Application
+     */
+    private $app;
+
+    /**
+     * @var WiseServiceProvider
+     */
+    private $provider;
+
+    public function testRegister()
+    {
+        $this->app->register($this->provider);
+        $this->app->boot();
+
+        $this->app['wise.cache_dir'] = $this->createDir();
+        $this->app['wise.path'] = $this->createDir();
+
+        file_put_contents(
+            $this->app['wise.path'] . '/test.ini',
+            <<<CONTENTS
+[imports]
+0[resource] = "test.php"
+
+[ini]
+test = 123
+CONTENTS
+        );
+
+        file_put_contents(
+            $this->app['wise.path'] . '/test.php',
+            <<<CONTENTS
+<?php return array(
+    'imports' => array(
+        array('resource' => 'test.json')
+    ),
+    'php' => array(
+        'test' => 456
+    )
+);
+CONTENTS
+        );
+
+        file_put_contents(
+            $this->app['wise.path'] . '/test.json',
+            <<<CONTENTS
+{
+    "imports": [
+        {
+            "resource": "test.xml"
+        }
+    ],
+    "json": {
+        "test": 789
+    }
+}
+CONTENTS
+        );
+
+        file_put_contents(
+            $this->app['wise.path'] . '/test.xml',
+            <<<CONTENTS
+<array>
+  <array key="imports">
+    <array>
+      <str key="resource">test.yml</str>
+    </array>
+  </array>
+  <array key="xml">
+    <int key="test">987</int>
+  </array>
+</array>
+CONTENTS
+        );
+
+        file_put_contents(
+            $this->app['wise.path'] . '/test.yml',
+            <<<CONTENTS
+yaml:
+    test: 654
+CONTENTS
+        );
+
+        $this->assertEquals(
+            array(
+                'imports' => array(
+                    array('resource' => 'test.yml')
+                ),
+                'ini' => array('test' => '123'),
+                'php' => array('test' => 456),
+                'json' => array('test' => 789),
+                'xml' => array('test' =>  987),
+                'yaml' => array('test' => 654),
+            ),
+            $this->app['wise']->load('test.ini')
+        );
+    }
+
+    protected function setUp()
+    {
+        $this->app = new Application(array('debug' => true));
+        $this->provider = new WiseServiceProvider();
+    }
+}
